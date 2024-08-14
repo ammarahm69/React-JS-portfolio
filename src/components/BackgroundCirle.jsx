8b5aef453c844310c71f1a15bd00fbe5090c2d72
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-14 12:44:14 -0700

    Hero-section-1 done

--- src/components/BackgroundCirle.jsx
+++ src/components/BackgroundCirle.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Image1 from "./bg-2.jpg";
+
 export default function BackgroundCirle() {
   return (
-    <motion.div
-    initial={{
-        opacity:0
-    }}
-    animate={{
-        scale:[1,1,2,2,3,1],
-        opacity:[0.9 ,0.7,0.5 ,0.8 ,0.9,0.5],
-        borderRadius:["20%" , "20%" ,"50%","80%" , "20%"]
-    }}
-    transition={{
-        duration:2.5
-    }}
-    className="relative flex justify-center items-center">
-      <div className="border border-[#333333] h-[200px] w-[200px] rounded-full mt-52 animate-ping" />
-      <div className="border [#333333] absolute h-[300px] w-[300px] rounded-full mt-52 animate-ping" />
-      <div className="border border-[#f5fd14] absolute h-[500px] w-[500px] rounded-full mt-52" />
-    </motion.div>
+    <>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          scale: [1, 1, 2, 2, 3, 1],
+          opacity: [0.9, 0.7, 0.5, 0.8, 0.9, 0.8],
+          borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+        }}
+        transition={{
+          duration: 2.5,
+        }}
+        className="relative flex justify-center items-center"
+      >
+        <div className="border border-[#333333] h-[200px] w-[200px] rounded-full  animate-ping" />
+        <div className="border [#333333] absolute h-[350px] w-[350px] rounded-full  animate-ping" />
+        <div className="border border-[#787575] border-2		border-r-white absolute  h-[550px] w-[550px] rounded-full animate-spin" />
+      </motion.div>
+      <div>
+        <img
+          src={Image1}
+          alt=""
+          className="w-44 h-44 rounded-full -mt-52 mx-auto"
+        />
+      </div>
+    </>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import BackgroundCirle from "./BackgroundCirle";
 function Hero() {
   const [text, count] = useTypewriter({
     words: [
-      "Hi, The Name's Ammar Ahmed",
+      "Hi,The Name's Ammar",
       "Guy-who-loves-Coffee.jsx",
       "<ButLovesToCodeMore />",
     ],
@@ -13,11 +13,21 @@ function Hero() {
   });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center text-center overflow-hidden mt-32">
+      <div className="mt-60">
         <BackgroundCirle />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A"/>
-      </h1>
+      </div>
+
+      <div>
+        <h2 className="text-md uppercase tracking-[8px] ms-8 text-gray-500">
+          Front End Developer
+        </h2>
+        <h1 className="lg:text-4xl mt-2 font-semibold md:text-2xl sm:text-2xl mr-2">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        {/* Button for About,Skills*/}
+      </div>
     </div>
   );
 }
