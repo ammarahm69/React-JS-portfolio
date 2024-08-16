598d446215dce4f857748384cc1f6d20b332a1c6
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-15 22:29:29 -0700

    project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Case1 from "./case-1.png";
 
 function Projects() {
+  const projects = [1, 2, 3, 4];
   return (
-    <div className="h-screen flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-3xl font-semibold">
+    <div className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center">
+      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-3xl font-semibold">
         Projects
       </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="w-[75%] flex-shrink-0 snap-center flex flex-col justify-center items-center space-y-5 p-10 md:p-16 lg:p-24 h-screen mt-20 min-[320px]:w-screen max-[770px]:w-screen"
+          >
+            <img
+              src={Case1}
+              alt={`Project ${project}`}
+              className="w-full h-auto object-cover"
+            />
+
+            <div>
+              <h4>Case Study {index + 1} of {projects.length}:</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/30 left-0 h-[500px] -skew-y-12"></div>
     </div>
   );
 }
