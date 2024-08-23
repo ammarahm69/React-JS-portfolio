1fd305450d935d1dc8b0e48cd787c717e73d9b16
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-22 23:43:05 -0700

    bg-changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import IconHome from "./components/home.webp";
 function App() {
   return (
     <Router>
-      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="bg-[rgb(28,26,26)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
 
         <Header />
 
